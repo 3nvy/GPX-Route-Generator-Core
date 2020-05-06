3d1ea24897153d2cee08deb93aeaa2a2af79fabc
3nvy <renatompestana@gmail.com>
2020-05-06 20:13:28 +0100

    Fix duplicated definition

--- 2opt.js
+++ 2opt.js
@@ -26,10 +26,10 @@ const calcCrow = (lat1, lon1, lat2, lon2) => {
   let R = 6371; // km
   let dLat = toRad(lat2 - lat1);
   let dLon = toRad(lon2 - lon1);
-  let lat1 = toRad(lat1);
-  let lat2 = toRad(lat2);
+  let l1 = toRad(lat1);
+  let l2 = toRad(lat2);
 
-  let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(l1) * Math.cos(l2);
   let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   let d = R * c;
   return d;
